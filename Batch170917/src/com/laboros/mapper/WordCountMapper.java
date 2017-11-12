d2af639f0994b5b086f4d3f2d08c6bfd9e9df2d1
svaduka <sainagaraju.vaduka@gmail.com>
2017-11-12 14:21:16 +0530

    Created WordCount MapReduce Program

---
+++ Batch170917/src/com/laboros/mapper/WordCountMapper.java
@@ -0,0 +1,29 @@
+package com.laboros.mapper;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends
+		Mapper<LongWritable, Text, Text, IntWritable> {
+
+	protected void setup(
+			Context context)
+			throws java.io.IOException, InterruptedException {
+	};
+
+	@Override
+	protected void map(
+			LongWritable key,
+			Text value,
+			Context context)
+			throws java.io.IOException, InterruptedException {
+	};
+
+	protected void cleanup(
+			Context context)
+			throws java.io.IOException, InterruptedException {
+	};
+
+}
---
+++ Batch170917/src/com/laboros/reducer/WordCountReducer.java
@@ -0,0 +1,27 @@
+package com.laboros.reducer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class WordCountReducer extends
+		Reducer<Text, IntWritable, Text, IntWritable> {
+
+	protected void setup(
+			Context context)
+			throws java.io.IOException, InterruptedException {
+	};
+
+	@Override
+	protected void reduce(
+			Text key,
+			java.lang.Iterable<IntWritable> values,
+			Context context)
+			throws java.io.IOException, InterruptedException {
+	};
+
+	protected void cleanup(
+			Context context)
+			throws java.io.IOException, InterruptedException {
+	};
+}
